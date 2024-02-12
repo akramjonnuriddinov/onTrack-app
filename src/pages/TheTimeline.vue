a4275cf082f73276e101f01ccb3081e50e14610c
akramjonnuriddinov <nn.akramjon@gmail.com>
2024-02-12 14:00:33 +0500

    Add style to TheTimeline component

--- src/pages/TheTimeline.vue
+++ src/pages/TheTimeline.vue
@@ -3,5 +3,19 @@
 </script>
 
 <template>
-  <h1>Timeline</h1>
+  <div class="mt-7">
+    <ul>
+      <li
+        v-for="i in [0, 1, 2, 3, 4, 5]"
+        :key="i"
+        class="relative flex flex-col gap-2 px-4 py-10 border-t border-gray-200"
+      >
+        <a
+          href="#"
+          class="absolute px-2 font-mono text-lg text-gray-500 -translate-x-1/2 bg-gray-100 rounded -top-4 left-1/2"
+          >{{ i }} : 00</a
+        >
+      </li>
+    </ul>
+  </div>
 </template>
