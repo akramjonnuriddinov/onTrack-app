3e675e762ea977f709fa3f28eeba7c71009674c3
akramjonnuriddinov <nn.akramjon@gmail.com>
2024-02-12 19:36:02 +0500

    Change style of TheTimeline

--- src/components/TheHeader.vue
+++ src/components/TheHeader.vue
@@ -6,7 +6,7 @@ const emit = defineEmits(['goToTimeline', 'goToProgress'])
 </script>
 
 <template>
-  <header class="sticky top-0 flex items-center justify-between p-3 bg-white border-b">
+  <header class="sticky top-0 z-50 flex items-center justify-between p-3 bg-white border-b">
     <TheLogo @click="emit('goToTimeline')" />
     <TheHeaderProgress @click="emit('goToProgress')" />
   </header>
--- src/pages/TheTimeline.vue
+++ src/pages/TheTimeline.vue
@@ -1,19 +1,23 @@
 <script setup>
-//
+const timelineItems = []
+
+for (let hour = 0; hour < 24; hour++) {
+  timelineItems.push({ hour })
+}
 </script>
 
 <template>
   <div class="mt-7">
     <ul>
       <li
-        v-for="i in [0, 1, 2, 3, 4, 5]"
-        :key="i"
+        v-for="{ hour } in timelineItems"
+        :key="hour"
         class="relative flex flex-col gap-2 px-4 py-10 border-t border-gray-200"
       >
         <a
           href="#"
           class="absolute px-2 font-mono text-lg text-gray-500 -translate-x-1/2 bg-gray-100 rounded -top-4 left-1/2"
-          >{{ i }} : 00</a
+          >{{ hour }} : 00</a
         >
       </li>
     </ul>
