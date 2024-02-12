5964c14a08470c81c970bcd26d90e2f8fa4a29ab
akramjonnuriddinov <nn.akramjon@gmail.com>
2024-02-12 12:51:26 +0500

    Add event to TheNav

--- src/components/TheNav.vue
+++ src/components/TheNav.vue
@@ -1,27 +1,15 @@
 <script setup>
-import { ref } from 'vue'
 import { ClockIcon, ListBulletIcon, ChartBarIcon } from '@heroicons/vue/24/outline'
 import { PAGE_TIMELINE, PAGE_ACTIVITIES, PAGE_PROGRESS } from '@/constants.js'
 import NavItem from '@/components/NavItem.vue'
+
+defineProps(['currentPage'])
+const emit = defineEmits(['navigate'])
 
 const navItems = {
   [PAGE_TIMELINE]: ClockIcon,
   [PAGE_ACTIVITIES]: ListBulletIcon,
   [PAGE_PROGRESS]: ChartBarIcon
-}
-
-const currentPage = ref(normalizePageHash())
-
-function normalizePageHash() {
-  const hash = window.location.hash.slice(1)
-
-  if (Object.keys(navItems).includes(hash)) {
-    return hash
-  }
-
-  window.location.hash = PAGE_TIMELINE
-
-  return PAGE_TIMELINE
 }
 </script>
 
@@ -33,7 +21,7 @@ function normalizePageHash() {
         :key="page"
         :page="page"
         :class="{ 'bg-gray-200 pointer-events-none': page === currentPage }"
-        @click="currentPage = page"
+        @click="emit('navigate', page)"
       >
         <component :is="icon" class="w-6 h-6" /> {{ page }}
       </NavItem>
